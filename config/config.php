cf46f55baaeab78df713e538750a6b4662fd5c83
Legacy <52163001+3m1n3nc3@users.noreply.github.com>
2025-01-19 06:08:16 +0100

    feat: Update config.php with instructions for mapped metadata.

--- config/config.php
+++ config/config.php
@@ -8,8 +8,17 @@ return [
     |
     | These configuration options determine what extra metadata will be
     | appended to your API response.
+    | Providing a map allows modification of the metadata keys.
     |
     | Supported options: "meta", "links"
+    |
+    | Examples
+    | 'paginated_response_extra' => ['meta', 'links'],
+    | - Or Mapped - 
+    | 'paginated_response_extra' => [
+    |    'meta' => 'metadata',
+    |    'links' => 'linkdata',
+    | ],
     |
     */
 
